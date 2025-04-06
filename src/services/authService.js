e11dcbf25cd505ea14ff8d430aff41643c5e72c7
MadMark <mqmadrid@gmail.com>
2025-04-06 13:55:03 -0400

    fornt back conection

---
+++ src/services/authService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API = import.meta.env.VITE_BACKEND_URL + "/api/auth";
+
+export const register = async (data) => {
+  const response = await axios.post(`${API}/register`, data);
+  return response.data;
+};
+
+export const login = async (data) => {
+  const response = await axios.post(`${API}/login`, data);
+  return response.data;
+};
+
+export const getProfile = async (token) => {
+  const response = await axios.get(`${API}/profile`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
